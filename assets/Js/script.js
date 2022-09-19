e84602d47dd1525b2f5b47bf0e4e39cdf83a1c8c
Lambo95 <lamberto.martini95@gmail.com>
2022-09-20 00:00:45 +0200

    carousel/mod

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -18,8 +18,8 @@ for (let li of carousel.querySelectorAll("li")) {
   i++;
 }
 
-let width = 276; // image width
-let count = 1; // visible images count
+let width = 276;
+let count = 1;
 
 let list = carousel.querySelector("ul");
 let listElems = carousel.querySelectorAll("li");
@@ -27,9 +27,7 @@ let listElems = carousel.querySelectorAll("li");
 let position = 0;
 
 carousel.querySelector(".next").onclick = function () {
-  // shift right
   position -= width * count;
-  // can only shift the ribbbon for (total ribbon length - visible count) images
   position = Math.max(position, -width * (listElems.length - count));
   list.style.marginLeft = position + "px";
 };
