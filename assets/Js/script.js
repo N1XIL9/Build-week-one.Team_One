1e3471537db0f4bbf0046ecd85541cd261789cd1
Lambo95 <lamberto.martini95@gmail.com>
2022-09-22 16:46:23 +0200

    update/query

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -2,6 +2,7 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 showSlidesQuery(slideIndex);
+showSlidesQueryBis(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -12,6 +13,9 @@ function plusSlidesQuery(n) {
   showSlidesQuery((slideIndex += n));
 }
 
+function plusSlidesQueryBis(n) {
+  showSlidesQueryBis((slideIndex += n));
+}
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
@@ -19,6 +23,10 @@ function currentSlide(n) {
 
 function currentSlideQuery(n) {
   showSlidesQuery((slideIndex = n));
+}
+
+function currentSlideQueryBis(n) {
+  showSlidesQueryBis((slideIndex = n));
 }
 
 function showSlides(n) {
@@ -45,6 +53,26 @@ function showSlidesQuery(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides-query");
   let dots = document.getElementsByClassName("dot-query");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlidesQueryBis(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides-query-bis");
+  let dots = document.getElementsByClassName("dot-query-bis");
   if (n > slides.length) {
     slideIndex = 1;
   }
