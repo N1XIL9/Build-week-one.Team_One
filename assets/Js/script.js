e4b97200ae362dff64959f7c360e57058ea7f2da
Lambo95 <lamberto.martini95@gmail.com>
2022-09-23 12:07:09 +0200

    update

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -86,12 +86,12 @@ function showSlidesQueryBis(n) {
 // ANIMAZIONE FADE SITO
 
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  let reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
